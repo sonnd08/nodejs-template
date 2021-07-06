200475a21c378733f079886a35db7bd81f66fc23
sonnd08 <sonnd@dgroup.co>
2021-07-06 12:22:39 +0700

    using dotenv to load .env file

--- routes/ping.js
+++ routes/ping.js
@@ -5,7 +5,7 @@ const router = Router();
 
 /* GET index page. */
 router.get('/', (req, res) => {
-  sendJson({ res, message: 'Pong' });
+  sendJson({ res, message: 'Pong', data: process.env.TEST_ENV });
 });
 
 export default router;
